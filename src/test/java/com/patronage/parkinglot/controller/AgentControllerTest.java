2e721fff2ebe849dfb844e1ac82e0ce55854e6dc
golldfish <a.karasinska13@gmail.com>
2021-11-25 00:25:12 +0100

    Added first test for AgentController and added dependencies to pom.xml

---
+++ src/test/java/com/patronage/parkinglot/controller/AgentControllerTest.java
@@ -0,0 +1,45 @@
+package com.patronage.parkinglot.controller;
+
+import com.patronage.parkinglot.model.DTO.AgentDTO;
+import com.patronage.parkinglot.service.AgentService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.hamcrest.core.Is.is;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(AgentController.class)
+public class AgentControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private AgentService agentService;
+
+    @Test
+    public void getAllAgents() throws Exception {
+        AgentDTO agentDTO = AgentDTO.builder().name("John").build();
+        List<AgentDTO> agents = Collections.singletonList(agentDTO);
+
+        when(agentService.getAgents()).thenReturn((agents));
+
+        this.mvc.perform(get("/agents"))
+                .andExpect(status().isOk()).andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].name", is(agentDTO.getName())));
+    }
+
+}
